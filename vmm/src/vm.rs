fa2c0bf2f60bda9513dcfe32bb32d933a57dcc5d
Michael Zhao <michael.zhao@arm.com>
2020-03-23 13:39:38 +0800

    Kernel loader builds.
    Signed-off-by: Michael Zhao <michael.zhao@arm.com>

--- vmm/src/vm.rs
+++ vmm/src/vm.rs
@@ -31,7 +31,6 @@ use crate::cpu;
 use crate::device_manager::{get_win_size, Console, DeviceManager, DeviceManagerError};
 use crate::memory_manager::{get_host_cpu_phys_bits, Error as MemoryManagerError, MemoryManager};
 use anyhow::anyhow;
-#[cfg(target_arch = "x86_64")]
 use arch::BootProtocol;
 use arch::{layout, EntryPoint};
 use devices::{ioapic, HotPlugNotificationFlags};
@@ -42,24 +41,21 @@ use kvm_bindings::{kvm_enable_cap, KVM_CAP_SPLIT_IRQCHIP};
 use kvm_ioctls::*;
 #[cfg(target_arch = "x86_64")]
 use linux_loader::cmdline::Cmdline;
-#[cfg(target_arch = "x86_64")]
 use linux_loader::loader::KernelLoader;
 use signal_hook::{iterator::Signals, SIGINT, SIGTERM, SIGWINCH};
 #[cfg(target_arch = "x86_64")]
 use std::ffi::CString;
 use std::fs::File;
 use std::io;
-#[cfg(target_arch = "x86_64")]
 use std::ops::Deref;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex, RwLock};
 use std::{result, str, thread};
 use vm_allocator::{GsiApic, SystemAllocator};
 use vm_device::{Migratable, MigratableError, Pausable, Snapshotable};
+use vm_memory::GuestAddressSpace;
 #[cfg(target_arch = "x86_64")]
-use vm_memory::{
-    Address, Bytes, GuestAddressSpace, GuestMemory, GuestMemoryMmap, GuestMemoryRegion,
-};
+use vm_memory::{Address, Bytes, GuestMemory, GuestMemoryMmap, GuestMemoryRegion};
 use vm_memory::{GuestAddress, GuestUsize};
 use vmm_sys_util::eventfd::EventFd;
 use vmm_sys_util::terminal::Terminal;
@@ -412,8 +408,39 @@ impl Vm {
 
     #[cfg(target_arch = "aarch64")]
     fn load_kernel(&mut self) -> Result<EntryPoint> {
-        println!("{:?}", self.kernel);
-        Err(Error::MemOverflow)
+        // Note, Aarch64 does not write the kernel command line to guest memory.
+        // The command line will be handled via FDT.
+        let guest_memory = self.memory_manager.lock().as_ref().unwrap().guest_memory();
+        let mem = guest_memory.memory();
+        let entry_addr = match linux_loader::loader::PE::load(
+            mem.deref(),
+            Some(GuestAddress(arch::get_kernel_start())),
+            &mut self.kernel,
+            None,
+        ) {
+            Ok(entry_addr) => entry_addr,
+            _ => panic!("Invalid elf file"),
+        };
+
+        let boot_vcpus = self.cpu_manager.lock().unwrap().boot_vcpus();
+        let _max_vcpus = self.cpu_manager.lock().unwrap().max_vcpus();
+
+        let mut entry_point_addr: GuestAddress = entry_addr.kernel_load;
+
+        let boot_prot = if entry_addr.pvh_entry_addr.is_some() {
+            // entry_addr.pvh_entry_addr field is safe to unwrap here
+            entry_point_addr = entry_addr.pvh_entry_addr.unwrap();
+            BootProtocol::PvhBoot
+        } else {
+            BootProtocol::LinuxBoot
+        };
+
+        arch::configure_system(&mem, boot_vcpus, boot_prot).map_err(Error::ConfigureSystem)?;
+
+        Ok(EntryPoint {
+            entry_addr: entry_point_addr,
+            protocol: boot_prot,
+        })
     }
 
     #[cfg(target_arch = "x86_64")]
