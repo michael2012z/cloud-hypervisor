99929b50874cb6e2880fd6487fd908df3053def8
Michael Zhao <michael.zhao@arm.com>
2020-04-02 15:48:50 +0800

    Simplify.
    Signed-off-by: Michael Zhao <michael.zhao@arm.com>

--- start_vm.sh
+++ start_vm.sh
@@ -1,2 +1,1 @@
-RUST_BACKTRACE=1 ./build/cargo_target/debug/cloud-hypervisor --kernel ./vmlinux.bin --disk path=rootfs.img --cmdline "console=hvc0 reboot=k panic=1 nomodules root=/dev/vda3" --cpus boot=4 --memory size=1024M --net "tap=,mac=,ip=,mask=" --rng --log-file log.log -vvv
-
+./build/cargo_target/debug/cloud-hypervisor --kernel ./hello-vmlinux.bin --disk path=hello-rootfs.ext4 --cmdline "keep_bootcon console=ttyS0 reboot=k panic=1 pci=off root=/dev/vda rw" --cpus boot=1 --memory size=128M --log-file log.log -vvv
